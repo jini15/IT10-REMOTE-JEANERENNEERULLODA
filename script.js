55db435c64134d08236f22cd89408545b2a82531
Jeane <rullodajeane@gmail.com>
2025-05-14 17:03:41 +0800

    Implemented font size change on selection

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ document.addEventListener("DOMContentLoaded", function() {
         displayText.style.fontFamily = fontSelect.ariaValueMax;
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const fontSelect = document.getElementById("fontSelect");
+    const sizeSelect = document.getElementById("sizeSelect");
+    const displayText = document.getElementById("displayText");
+
+    fontSelect.addEventListener("change", function() {
+        displayText.style.fontFamily = fontSelect.value;
+    });
+
+    sizeSelect.addEventListener("change", function() {
+        displayText.style.fontFamily = sizeSelect.value;
+    });
+});
